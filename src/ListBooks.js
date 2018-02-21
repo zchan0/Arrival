3363054bf86873101e0b98f2327c82b3589afe23
Cc Zheng <zchan0@outlook.com>
2018-02-22 00:16:15 +0800

    Remove mockup & Add route

---
+++ src/ListBooks.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ListBooks extends Component {
+  render() {
+    return (
+      <div className="list-books">
+        ListBooks
+      </div>
+    )
+  }
+}
+
+export default ListBooks
---
+++ src/SearchBooks.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SearchBooks extends Component {
+  render() {
+    return (
+      <div className="search-books">
+        SearchBooks
+      </div>
+    )
+  }
+}
+
+export default SearchBooks
